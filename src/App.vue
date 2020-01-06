be6d925ff3d315c664119fc1f3df9cb2dd85e956
liuqinggang <1542574711@qq.com>
2020-01-06 20:32:42 +0800

    项目初始化-封装axios请求函数

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div id="app">
     <!-- 跟路由出口 -->
-    <div class="box">
-      娃哈哈whh
-    </div>
+
     <router-view></router-view>
   </div>
 </template>
---
+++ src/utils/request.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+const request = axios.created({
+  baseURL: 'http://ttapi.research.itcast.cn/'
+})
+export default request
