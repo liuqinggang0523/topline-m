5b9e28efcbd0d38d6d3828af82fec3da0dce03db
liuqinggang <1542574711@qq.com>
2020-01-06 20:48:04 +0800

    项目初始化-处理失真大数字

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
+
+import jsonBigint from 'json-bigint'
 const request = axios.created({
   baseURL: 'http://ttapi.research.itcast.cn/'
 })
+// 处理失真大数字
+request.defaults.transformResponse = [function (data) {
+  return data ? jsonBigint.parse(data) : {}
+}]
 export default request
