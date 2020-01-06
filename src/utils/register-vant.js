8794fe9e3a3fcdf9de47fa566db31e54da1253a8
liuqinggang <1542574711@qq.com>
2020-01-06 21:45:56 +0800

    登录页面-搭建页面结构完成

--- src/utils/register-vant.js
+++ src/utils/register-vant.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
-import { Button, Popup, Field } from 'vant'
+import { Button, Popup, Field, NavBar, Icon } from 'vant'
 Vue.use(Button)
   .use(Popup)
   .use(Field)
+  .use(NavBar)
+  .use(Icon)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,48 @@
 <template>
-  <div>登录</div>
+  <div class="continaer">
+    <van-nav-bar
+      title="登录"
+    />
+    <van-cell-group>
+      <van-field
+      v-model="user.mobile"
+      required
+      label="手机号"
+      left-icon="phone-o"
+      placeholder="请输入手机号"
+      />
+      <van-field
+      v-model="user.code"
+      required
+      label="密码"
+      left-icon="envelop-o"
+      placeholder="请输入验证码"
+      />
+    </van-cell-group>
+    <div class="btn">
+      <van-button type="info" class="login-btn">登录</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scope>
+  .btn{
+    padding:20px;
+    .login-btn{
+      width: 100%;
+    }
+  }
 </style>
