7ea284b7c5f81c87a0afabca9b9526c307ebb321
liuqinggang <1542574711@qq.com>
2020-01-06 20:04:13 +0800

    项目初始化-样式-less文件结构

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="app">
     <!-- 跟路由出口 -->
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <van-cell-group>
-    <van-field v-model="value" placeholder="请输入用户名" />
-    </van-cell-group>
     <router-view></router-view>
   </div>
 </template>
