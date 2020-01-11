f893f66445b53534aae3fa8715afd540372fe164
liuqinggang <1542574711@qq.com>
2020-01-11 21:55:49 +0800

    首页-频道弹窗-删除我的频道

--- src/views/homepage/components/channeledit.vue
+++ src/views/homepage/components/channeledit.vue
@@ -2,14 +2,28 @@
   <div class="channels-edit">
     <van-cell title="我的频道" :border="false">
       <!-- 使用 right-icon 插槽来自定义右侧图标 -->
-      <van-button slot="right-icon" size="mini" round type="danger">编辑</van-button>
+      <van-button
+        slot="right-icon"
+        size="mini"
+        round
+        type="danger"
+        @click="isEditShow=!isEditShow"
+      >{{isEditShow?'完成':'编辑'}}</van-button>
     </van-cell>
     <van-grid :gutter="10">
       <van-grid-item
-        v-for="channel in userChannels"
+        v-for="(channel,index) in userChannels"
         :key="channel.id"
         :text="channel.name"
-      />
+        @click="onUserChannelClick(index)"
+      >
+        <van-icon
+          v-show="isEditShow"
+          class="close-icon"
+          slot="icon"
+          name="close"
+        />
+      </van-grid-item>
     </van-grid>
     <van-cell title="推荐频道" :border="false" />
     <van-grid :gutter="10">
@@ -29,7 +43,8 @@ export default {
   name: 'ChannelsEdit',
   data () {
     return {
-      list: [] // 所有频道
+      list: [], // 所有频道
+      isEditShow: false // 是否编辑状态
     }
   },
   props: {
@@ -39,6 +54,11 @@ export default {
     }
   },
   methods: {
+    onUserChannelClick (index) {
+      if (this.isEditShow) { // 判断是编辑状态还是未编辑状态
+        this.userChannels.splice(index, 1)
+      }
+    },
     async onlondChannels () { // 所有频道
       const { data } = await getChannelsList()
       this.list = data.data.channels
@@ -67,5 +87,16 @@ export default {
 <style lang="less" scoped>
   .channels-edit{
     padding: 40px 0;
+  /deep/.van-grid-item__content{
+      position: relative;
+     .van-grid-item__icon-wrapper{
+        position: absolute;
+        top: -12px;
+        right: -5px;
+        .close-icon{
+          font-size: 18px;
+        }
+      }
+    }
   }
 </style>
