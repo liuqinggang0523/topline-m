ba660e8dad921f780919fe392b392b56af900cd2
liuqinggang <1542574711@qq.com>
2020-02-05 20:14:22 +0800

    组件缓冲

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
     <!-- 跟路由出口 -->
-    <router-view></router-view>
+    <!-- 组件缓存 -->
+    <keep-alive :include="$store.state.cachePage">
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      cachePage: ['TabBar']
+    }
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -7,7 +7,8 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    user: getItem('user') // 一个对象，包含用户登录的token信息
+    user: getItem('user'), // 一个对象，包含用户登录的token信息
+    cachePage: ['TabBar']
   },
   mutations: {
     setUser (state, data) {
@@ -16,6 +17,19 @@ export default new Vuex.Store({
         data.id = decodeJwt(data.token).user_id
       }
       setItem('user', state.user)
+    },
+    // 添加缓存页面
+    addCachePage (state, name) {
+      if (!state.cachePage.includes(name)) {
+        state.cachePage.push(name)
+      }
+    },
+    // 移除缓存页面
+    removeCachePage (state, name) {
+      const index = state.cachePage.indexOf(name)
+      if (index !== -1) {
+        state.cachePage.splice(index)
+      }
     }
   },
   actions: {
